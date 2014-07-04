46c5fab7a1bb9189df950ece72e0d3ceb8740041
Carl Svensson <csvenss2@gmail.com>
2014-07-03 22:01:32 -0700

    Added command line options and scraping for historical data (2004-2013)
    mls_scraper.rb -y 2013 -o mls_2013.txt

--- scripts/mls_scraper.rb
+++ scripts/mls_scraper.rb
@@ -1,32 +1,100 @@
 require 'open-uri'
-require 'nokogiri'
+#require 'nokogiri'
 require 'logger'
 require 'net/http'
 # Jaavscript parser
-require 'rkelly'
+require 'optparse'
 
 # A command-line utility to parse MLS data into
 # openfootball fixtures
 class MLSScraper
   # Initialize logging
   # TBD: Determine source?
-  def initialize(log_level=Logger::WARN)
+  def initialize(options)
     @logger = Logger.new(STDOUT)
-    @logger.level = log_level
+    @logger.level = (options[:verbose] != nil) ? options[:verbose] : Logger::WARN
+    @logger.debug(options.to_s)
+
+    @options = options
   end
 
-  def scrape
-    doc = Nokogiri::HTML(open("http://data2.7m.cn/matches_data/107/en/index.shtml"))
+  # Slightly improved JS array parsing
+  # Requires array be all on one line
+  # Allows for 2D arrays
+  def parse_js_array_v2(js)
+    lines = js.split(/\r\n/)
+    arrays = []
 
-    url = URI.parse(URI.encode('http://data2.7m.cn/matches_data/107/en/fixture.js'))
-    request = Net::HTTP::Get.new(url.path, {'Accept' => '*/*', 'Cache-Control' => 'max-age=0', 'If-Modified-Since' => 'Mon, 30 Jun 2014 02:40:09 GMT', 'If-None-Match' => "a0b2449cc94cf1:0", 'Referer' => 'http://data2.7m.cn/matches_data/107/en/index.shtml'})
+    lines.each do |line|
+      # Parse out the name index and array
+      match = /([0-9A-Za-z_\-]+)\[?(\d?\d?)\]?\s*=\s*(\[.*\])/.match(line)
+      
+      if (match.kind_of?(MatchData))
+        @logger.info("[parse_js_array_v2] " + match.to_s)
+        arr = {}
+        matched = false
+        arr[:name] = match[1]
+        arr[:val] = eval(match[3])
+
+        # Check if the name already exists.  If so, it will become an array of arrays (2D)
+        arrays.each do |a|
+          if (a[:name] == arr[:name])
+            matched = true
+            a[:val][match[2].to_i] = eval(match[3])
+          end
+        end
+
+        # If it doesn't exist, set the value directly
+        if (not matched)
+          arrays.push(arr)
+        end
+      end # end match
+    end # end each
+
+    return arrays
+  end
+
+  # Format a URL for historical data, and return a request
+  def get_historical_game_data(year)
+    url_s = "http://data2.7m.cn/history_matches_data/#{year}/107/en/matches.js"
+    url = URI.parse(URI.encode(url_s))
+    request = Net::HTTP::Get.new(url.path, {'Accept' => '*/*', 'Cache-Control' => 'max-age=0', 
+      'If-Modified-Since' => 'Mon, 30 Jun 2014 02:40:09 GMT', 'If-None-Match' => "bcb08f55c6f3ce1:bed4", 
+      'Referer' => "http://data2.7m.cn/history_matches_data/#{year}/107/en/index.shtml"})
+
+    js = get_response(url, request)
+    @logger.debug("[get_historical_game_data] Data Response: " + js)
+
+    # These arrays have a different format
+    # s_name_arr = [ 'Qualifying','Play-offs','Quarter Final','Semifinal','Final'];
+    # Some arrays are 2D, with first index corresponding to s_name_arr index
+    arrays = parse_js_array_v2(js)
+    return parse_arrays(arrays)
+  end
+
+  def get_response(url, request)
     response = Net::HTTP.start(url.host,url.port) { |http|
       http.request(request)
     }
 
-    js = response.body
+    return response.body
+  end
+
+  def scrape
+    #doc = Nokogiri::HTML(open("http://data2.7m.cn/matches_data/107/en/index.shtml"))
+
+    url = URI.parse(URI.encode('http://data2.7m.cn/matches_data/107/en/fixture.js'))
+    request = Net::HTTP::Get.new(url.path, {'Accept' => '*/*', 'Cache-Control' => 'max-age=0', 'If-Modified-Since' => 'Mon, 30 Jun 2014 02:40:09 GMT', 'If-None-Match' => "a0b2449cc94cf1:0", 'Referer' => 'http://data2.7m.cn/matches_data/107/en/index.shtml'})
+
+    js = get_response(url,request)
     @logger.debug("[scrape] Data Response: " + js)
 
+    arrays = js_to_ruby(js)
+    return parse_arrays(arrays)
+  end
+
+  # Convert javascript arrays, +js+, into ruby arrays
+  def js_to_ruby(js)
     # Split response into variables
     vars = js.split('var')
     @logger.debug("[scrape] vars: " + vars.to_s)
@@ -37,6 +105,14 @@ class MLSScraper
       arrays.push(parse_js_array(var.strip))
     end
 
+    return arrays
+  end
+
+  # Take a hash of javascript +arrays+ and find
+  # the ones with useful information about the game
+  #
+  # Return a string representing the parsed fixture
+  def parse_arrays(arrays)
     # Time_Arr is time for each game - what time zone?
     #  3.9.2014 4h25m
     #  3.9.2014 8h00m - 3.8.2014 19:00 EST - 13 hours ahead of EST UTC+1?
@@ -45,29 +121,43 @@ class MLSScraper
     teamA = []
     teamB = []
     scores = []
+    rounds = []
+    ret = ""
 
     arrays.each do |arr|
       case(arr[:name])
-        when "Time_Arr"
+        when "Start_time_arr", "Time_Arr"
           times = arr[:val]
           @logger.debug("[scrape] times: " + times.to_s)
-        when "TeamA_Arr"
+        when "TeamA_Arr", "TeamA_arr"
           teamA = arr[:val]
           @logger.debug("[scrape] teamA: " + teamA.to_s)
-        when "TeamB_Arr"
+        when "TeamB_Arr", "TeamB_arr"
           teamB = arr[:val]
           @logger.debug("[scrape] teamB: " + teamB.to_s)
-        when "Scores_Arr"
+        when "score_arr", "Scores_Arr"
           scores = arr[:val]
           @logger.debug("[scrape] scores: " + scores.to_s)
+        when "s_name_arr"
+          rounds = arr[:val]
+          @logger.debug("[scrape] rounds: " + rounds.to_s)
       end
     end
 
-    return print_fixture(times,teamA,teamB,scores)
-    # Sample CSS
-    #match_table = doc.css('#Match_Table table')
-    # Sample XPATH
-    #details["name"] = doc.search('//meta[@itemprop="name"]').first['content']
+    # Different queries will have different formats
+    #   Historical game data will have 2D arrays organized into rounds
+    #   s_name_arr holds the rounds.  If this exists, we will parse the fixture
+    #   into rounds
+    if (rounds.size > 0)
+      rounds.each_with_index do |round, i|
+        ret += print_fixture(rounds[i],times[i],teamA[i],teamB[i],scores[i]) + "\n"
+      end
+    else
+      # Otherwise, assume current year data
+      ret = print_fixture("Qualifying",times,teamA,teamB,scores)
+    end
+
+    return ret
   end
 
   # Return a hash {name: "foo", val: [bar]}
@@ -105,7 +195,7 @@ class MLSScraper
   # to openfootball fixture format
   #  Example:
   #  Sun Mar/9 D.C. United            0-3   Columbus Crew
-  def print_fixture(times, teamA, teamB, scores)
+  def print_fixture(round, times, teamA, teamB, scores)
     # check parameters
     if (times.size != teamA.size or teamA.size != teamB.size or teamB.size != scores.size or teamB.size != times.size)
       @logger.warn("[parse_fixture] Arrays are not all the same size!")
@@ -115,7 +205,7 @@ class MLSScraper
       @logger.debug("[parse_fixture]     scores.size: " + scores.size.to_s)
     end
 
-    ret_s = ""
+    ret_s = "#{round}\n\n"
 
     times.each_with_index do |t, i|
       # convert time to proper format
@@ -136,15 +226,55 @@ class MLSScraper
     return ret_s
   end
 
-  def self.main
-    m = MLSScraper.new
-    fixture_data = m.scrape
-    File.open("tmp.txt", 'w') {|file| file.write(fixture_data)}
+
+end
+
+# Parse the command-line
+def parse_options
+  options = {}
+
+  # Some defaults
+  options[:file] = "temp.txt"
+  options[:verbose] = Logger::WARN
+  options[:year] = 2014
+
+  opts = OptionParser.new do |opt|
+    opt.banner = "Usage: mls_scraper.rb [-o FILE -y YEAR -d games|teams|roster -v]"
+
+    opt.on('-v', '--verbose', 'Print debug information') do 
+      options[:verbose] = Logger::DEBUG
+    end
+
+    opt.on('-o', '--output FILE', 'Output resuls to FILE') do |file|
+      options[:file] = file
+    end
+
+    opt.on('-y', '--year YEAR', 'Get historical data for YEAR') do |year|
+      options[:year] = year
+    end
   end
+
+  opts.parse!
+  return options
+end
+
+# Parse options and start the scraping
+def main
+  options = parse_options
+  fixture_data = ""
+  m = MLSScraper.new(options)
+
+  # Current year is a different API
+  if (options[:year].to_i == 2014)
+    fixture_data = m.scrape
+  else
+    fixture_data = m.get_historical_game_data(options[:year])
+  end
+  File.open(options[:file], 'w') {|file| file.write(fixture_data)}
 end
 
 # Execute standalone or import
 if __FILE__ == $0
-  MLSScraper.main
+ main
 end
 
